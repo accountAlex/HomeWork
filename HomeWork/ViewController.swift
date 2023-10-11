11ce70fa6c046574ba6a4e1cd13771f965b9ccad
Alex <57699618+accountAlex@users.noreply.github.com>
2023-10-11 23:49:29 +0300

    final

--- HomeWork/ViewController.swift
+++ HomeWork/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let images: [UIImage] = [
+        UIImage(named: "ball1")!,
+        UIImage(named: "ball2")!,
+        UIImage(named: "ball3")!,
+        UIImage(named: "ball4")!,
+        UIImage(named: "ball5")!
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.image = UIImage(named: "ball1")
+        button.tintColor = .black
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        imageView.image = images.randomElement()!
+    }
 }
+
 
